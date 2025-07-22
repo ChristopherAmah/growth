983e2b8f6cca6f7a100ede5192cf2081660777c5
Christopher Amah <christopheramah0@gmail.com>
2025-07-22 10:20:24 +0100

    framer-motion added

--- src/components/NewsletterSection.jsx
+++ src/components/NewsletterSection.jsx
@@ -3,7 +3,7 @@ import { HiArrowRight } from 'react-icons/hi';
 
 const NewsletterSection = () => {
   return (
-    <section className='container mx-auto px-4 sm:px-6 lg:px-8 py-16'>
+    <section className='container mx-auto px-4 sm:px-6 lg:px-8 py-16' id='newsletter'>
       <div className='bg-blue-600 rounded-2xl overflow-hidden relative'>
         {/* gradient background layer */}
         <div className='absolute top-0 right-0 w-1/2 h-full bg-blue-700 clip-path-slant hidden md:block z-0'></div>
--- src/components/TestimonialSection.jsx
+++ src/components/TestimonialSection.jsx
@@ -50,7 +50,7 @@ const testimonials = [
 
 const TestimonialSection = () => {
   return (
-    <section className='py-26 px-4 max-w-7xl mx-auto'>
+    <section className='py-26 px-4 max-w-7xl mx-auto' id='testimonials'>
         <div className='text-center '>
             <h2 className='text-3xl font-bold md:text-4xl mb-4'>What our happy client say</h2>
             <p className='text-gray-600'>Things that make it the best place to start trading</p>
